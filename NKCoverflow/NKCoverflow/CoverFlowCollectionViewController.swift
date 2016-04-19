2b3a314f1303befcb1c448d3a8be73af412ca2a3
Nick Kibish <kibish@steelkiwi.com>
2016-04-19 22:37:01 +0300

    created project

---
+++ NKCoverflow/NKCoverflow/CoverFlowCollectionViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CoverFlowCollectionViewController.swift
+//  NKCoverflow
+//
+//  Created by Nick Kibish on 4/19/16.
+//  Copyright © 2016 Nick Kibish. All rights reserved.
+//
+
+import UIKit
+
+class CoverFlowCollectionViewController: UICollectionViewController {
+
+}
---
+++ NKCoverflow/NKCoverflow/NKCoverFlowCollectionViewLayout.swift
@@ -0,0 +1,13 @@
+//
+//  NKCoverFlowCollectionViewLayout.swift
+//  NKCoverflow
+//
+//  Created by Nick Kibish on 4/19/16.
+//  Copyright © 2016 Nick Kibish. All rights reserved.
+//
+
+import UIKit
+
+class NKCoverFlowCollectionViewLayout: UICollectionViewLayout {
+
+}
